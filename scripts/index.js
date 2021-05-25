d4fb45c65c775b3b2b4c876f0c5587d2f6bd1acd
Milka Code <63292500+MilkaCode@users.noreply.github.com>
2021-05-25 14:58:42 +0200

    Code | Artin

---
+++ scripts/index.js
@@ -0,0 +1,53 @@
+function changeGithub() {
+	document.getElementById("button-github").innerHTML = "GitHub";
+}
+
+function changeProjects() {
+	document.getElementById("button-projects").innerHTML = "Projects";
+}
+
+function changeAboutMe() {
+	document.getElementById("button-about-me").innerHTML = "About Me";
+}
+
+function changeWelcome() {
+	document.getElementById("button-welcome").innerHTML = "Home";
+}
+
+function changeTiktok() {
+	document.getElementById("button-tiktok").innerHTML = "Dedicated TikTok Page";
+}
+
+function changeDiscord() {
+	document.getElementById("button-discord").innerHTML = "My Discord";
+}
+
+function changeGithubIcon() {
+	document.getElementById("button-github").innerHTML =
+		'<i class="fab fa-github"></i>';
+}
+
+function changeProjectsIcon() {
+	document.getElementById("button-projects").innerHTML =
+		'<i class="fas fa-terminal"></i>';
+}
+
+function changeWelcomeIcon() {
+	document.getElementById("button-welcome").innerHTML =
+		'<i class="fas fa-door-open"></i>';
+}
+
+function changeAboutMeIcon() {
+	document.getElementById("button-about-me").innerHTML =
+		'<i class="fas fa-user"></i>';
+}
+
+function changeTiktokIcon() {
+	document.getElementById("button-tiktok").innerHTML =
+		'<i class="fab fa-tiktok"></i>';
+}
+
+function changeDiscordIcon() {
+	document.getElementById("button-discord").innerHTML =
+		'<i class="fab fa-discord"></i>';
+}
